81b1bf35c081bf0f79899925515b6f85e3a7f020
lzjchina <1042598114@QQ.com>
2018-07-12 16:44:37 +0800

    首页

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$('.downMenu li a').on('touchend', function () {
+	$(this).css('background', '#b48845');
+	$(this).parent().siblings().children().css('background', 'none');
+});
+
+// 下拉菜单
+$('.downMenuIcon').on('click, touchend', function () {
+	$(this).hide();
+	$('.downMenuBox').slideToggle();
+});
+
+$('.menuClose').on('click, touchend', function () {
+	$('.downMenuIcon').show();
+	$('.downMenuBox').slideToggle();
+});
+
+// 京华动态菜单
+$('.kingChina_nav').on('click, touchend', function () {
+	$(this).addClass('active').siblings().removeClass('active');
+	$(this).children('.triangle_border_down').show();
+	$(this).siblings().children('.triangle_border_down').hide();
+});
